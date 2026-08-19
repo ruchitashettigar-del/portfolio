697c6138271565c7e7d084cb0cd2e9fe8b8fbae2
ruchitashettigar-del <ruchitashettigar@gmail.com>
2026-08-19 19:27:15 +0530

    Update java.js

--- java.js
+++ java.js
@@ -50,3 +50,47 @@ setTimeout(type,500);
 
 
 type();
+const typing = document.getElementById("typing");
+
+const words = [
+    "Computer Science Student",
+    "Web Developer",
+    "Programmer",
+    "Creative Learner"
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+let deleting = false;
+
+function typeEffect() {
+
+    const currentWord = words[wordIndex];
+
+    if (!deleting) {
+        typing.textContent = currentWord.substring(0, charIndex + 1);
+        charIndex++;
+
+        if (charIndex === currentWord.length) {
+            deleting = true;
+            setTimeout(typeEffect, 1500);
+            return;
+        }
+    } else {
+        typing.textContent = currentWord.substring(0, charIndex - 1);
+        charIndex--;
+
+        if (charIndex === 0) {
+            deleting = false;
+            wordIndex++;
+
+            if (wordIndex === words.length) {
+                wordIndex = 0;
+            }
+        }
+    }
+
+    setTimeout(typeEffect, deleting ? 60 : 100);
+}
+
+typeEffect();
